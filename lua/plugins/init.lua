3cf1fdd3ab6c49b485865327b6935418d14f2c6c
ZaraPhu <zara.phukan@gmail.com>
2025-05-03 14:06:07 -0300

    Added lazygit plugin

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -3,6 +3,23 @@ return {
 	{ "nvim-treesitter/nvim-treesitter" },
 	{ "nvim-telescope/telescope.nvim", tag = '0.1.8' },
 	{ "ThePrimeagen/harpoon", branch = "harpoon2" },
-    { "aserowy/tmux.nvim", config = function() return require("tmux").setup() end },
-    { "neovim/nvim-lspconfig" }
-}
+  { "aserowy/tmux.nvim", config = function() return require("tmux").setup() end },
+  { "neovim/nvim-lspconfig" },
+  {
+    "kdheepak/lazygit.nvim",
+    lazy = true,
+    cmd = {
+        "LazyGit",
+        "LazyGitConfig",
+        "LazyGitCurrentFile",
+        "LazyGitFilter",
+        "LazyGitFilterCurrentFile",
+      },
+      dependencies = {
+        "nvim-lua/plenary.nvim",
+      },
+      keys = {
+        { "<leader>lg", "<cmd>LazyGit<cr>", desc = "LazyGit" }
+      }
+    }
+  }
--- lua/zara/settings.lua
+++ lua/zara/settings.lua
@@ -5,7 +5,7 @@ opt.number = true
 opt.relativenumber = true
 opt.termguicolors = true
 
-opt.tabstop = 4
-opt.shiftwidth = 4
+opt.tabstop = 2
+opt.shiftwidth = 2
 opt.expandtab = true
 
